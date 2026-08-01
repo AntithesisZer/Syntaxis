9096b5d00530d4187468d5490632ae92c9517ad6
Unknown <zeranexhercy@gmail.com>
2026-08-01 12:36:04 +0800

    Syntaxis

---
+++ syntaxis_snippets.js
@@ -0,0 +1,15 @@
+[
+    
+    // Math Block
+    {
+        trigger: "$",
+        replacement: "$$0$",
+        options: "t"
+    },
+    {
+        trigger: "$$",
+        replacement: "$$\n$0\n$$$1",
+        options: "t"
+    }
+
+]
